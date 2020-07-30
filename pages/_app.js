1c6932757abd8c84cd49cb30f78fe1b92d819252
Nick Radford <nick@nicholasradford.com>
2020-07-30 13:31:47 -0700

    host/serve merriweather instead of using google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        />
-
         <link rel="icon" href="/favicon.ico" />
 
         {/* <!-- Primary Meta Tags --> */}
