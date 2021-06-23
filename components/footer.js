731e0cb612c9a8bdf60cd5cbc8660266a6bc3346
Nick Radford <nick@nicholasradford.com>
2021-06-22 21:37:45 -0700

    pb-safe

--- components/footer.js
+++ components/footer.js
@@ -1,26 +1,24 @@
 import { LinkButton } from "./button";
 
 const Footer = () => (
-  <footer className="text-white font-scp p-4 bg-black bg-opacity-70 w-full">
-    <span classname="pb-safe">
-      <div className="max-w-3xl m-auto grid grid-cols-3 items-center">
-        <h4 className="text-lg col-span-2">© Nick Radford 2021</h4>
-        <nav className="">
-          <ul className="sm:grid grid-cols-2 gap-2 hidden">
-            <li className="flex-1">
-              <LinkButton href="https://www.linkedin.com/in/nickradford">
-                LinkedIn
-              </LinkButton>
-            </li>
-            <li className="flex-1">
-              <LinkButton href="https://github.com/nickradford">
-                GitHub
-              </LinkButton>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </span>
+  <footer className="text-white font-scp p-4 bg-black bg-opacity-70 w-full pb-safe">
+    <div className="max-w-3xl m-auto grid grid-cols-3 items-center">
+      <h4 className="text-lg col-span-2">© Nick Radford 2021</h4>
+      <nav className="">
+        <ul className="sm:grid grid-cols-2 gap-2 hidden">
+          <li className="flex-1">
+            <LinkButton href="https://www.linkedin.com/in/nickradford">
+              LinkedIn
+            </LinkButton>
+          </li>
+          <li className="flex-1">
+            <LinkButton href="https://github.com/nickradford">
+              GitHub
+            </LinkButton>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </footer>
 );
 
