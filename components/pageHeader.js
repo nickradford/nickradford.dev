16e3349cac867759efd8f66342ac292e457b79bf
Nick Radford <nick@nicholasradford.com>
2021-06-22 20:57:44 -0700

    hide menu when clicking the header

--- components/pageHeader.js
+++ components/pageHeader.js
@@ -18,7 +18,7 @@ const PageHeader = () => {
         <header className="text-2xl relative text-white font-scp bg-black bg-opacity-60 backdrop-filter backdrop-blur-md px-4 py-3 w-screen shadow z-10">
           <div className="bg-red-60 max-w-3xl m-auto flex justify-between items-center">
             <Link href="/">
-              <a className="">
+              <a className="" onClick={() => setMenuShowing(false)}>
                 <h1 style={{ textShadow: "0 0 6px rgba(255, 255, 255, .75)" }}>
                   Nick Radford
                 </h1>
