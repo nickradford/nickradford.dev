bffccbbef118aeead93633dfcb745065ec0a238d
Nick Radford <nick@nicholasradford.com>
2021-07-29 22:59:16 -0700

    add response header?

--- next.config.js
+++ next.config.js
@@ -2,4 +2,18 @@ module.exports = {
   images: {
     domains: ["images.ctfassets.net"],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/api/generate-preview-image",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/png",
+          },
+        ],
+      },
+    ];
+  },
 };
