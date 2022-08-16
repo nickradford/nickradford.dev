9e0483103dc4fb6c70fa244a214843de69366ba2
Nick Radford <nick@nicholasradford.com>
2022-08-15 21:46:46 -0700

    Fix autoprefixer

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,13 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
+    // noinspection JSUnresolvedLibraryURL,HtmlRequiredTitleElement
     return (
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100,400,800&family=Source+Code+Pro&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;800&family=Source+Code+Pro&display=swap"
             rel="stylesheet"
           />
           <script
