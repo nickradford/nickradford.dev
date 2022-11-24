2e9314df643d9d6d13fd952a317ba658df33c426
Nick Radford <nick@nicholasradford.com>
2022-11-23 16:16:12 -0800

    fix footer at small screen sizes

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import { LinkButton } from "./button";
 
 const Footer = () => (
   <footer className="z-10 w-full p-4 text-text font-scp pb-safe bg-gradient-to-t from-crust">
-    <div className="grid items-center max-w-4xl grid-cols-3 m-auto">
+    <div className="flex flex-col-reverse items-center max-w-4xl gap-4 m-auto md:flex-row md:justify-between">
       <h4 className="col-span-2 text-lg">© Nick Radford 2022</h4>
       <nav className="">
         <ul className="flex justify-end gap-6">
