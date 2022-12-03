c984f335f88c4c36da29e61fb18a1dfc3f97b271
Nick Radford <nick@nicholasradford.com>
2022-12-03 03:30:32 -0800

    remove mdx import for now

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="z-10 w-full py-8 mt-8 border-t border-zinc-800/75">
-      <ul className="flex gap-8">
+    <footer className="z-10 flex items-center justify-between w-full py-8 mt-8 border-t border-zinc-800/75">
+      <ul className="flex gap-8 text-sm font-semibold text-zinc-400">
         <li>Mastodon</li>
         <li>GitHub</li>
       </ul>
+      <span className="text-sm text-zinc-400">
+        &copy; {new Date().getFullYear()} Nick Radford, All rights reserved.
+      </span>
     </footer>
   );
 }
