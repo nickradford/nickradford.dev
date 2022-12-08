259c894fc82382010d4a2f6f4ff2ea612230f633
Nick Radford <nick@nicholasradford.com>
2022-12-08 09:01:59 -0800

    Fix issue when Spotify is playing something other than a song

--- hooks/useNowPlaying.ts
+++ hooks/useNowPlaying.ts
@@ -25,6 +25,7 @@ type NowPlaying = {
   };
   is_playing: boolean;
   progress_ms: number;
+  currently_playing_type: "track" | "episode";
 };
 
 export function useNowPlaying({
@@ -40,23 +41,25 @@ export function useNowPlaying({
   });
 
   const isPlaying = song?.is_playing;
-  const title = song?.item.name;
-  const artist = song?.item.artists.map((_artist) => _artist.name).join(", ");
-  const album = song?.item.album.name;
-  const albumImageUrl = song?.item.album.images[0].url;
+  const nowPlayingType = song?.currently_playing_type;
+  const title = song?.item?.name;
+  const artist = song?.item?.artists.map((_artist) => _artist.name).join(", ");
+  const album = song?.item?.album.name;
+  const albumImageUrl = song?.item?.album.images[0].url;
   const albumImageDimensions = {
-    width: song?.item.album.images[0].width,
-    height: song?.item.album.images[0].height,
+    width: song?.item?.album.images[0].width,
+    height: song?.item?.album.images[0].height,
   };
-  const songUrl = song?.item.external_urls.spotify;
+  const songUrl = song?.item?.external_urls.spotify;
   const currentProgress = song?.progress_ms;
-  const duration = song?.item.duration_ms;
+  const duration = song?.item?.duration_ms;
   const progress = (currentProgress / duration) * 100;
-  const id = song?.item.id;
+  const id = song?.item?.id;
 
   return {
     nowPlaying: {
       isPlaying,
+      nowPlayingType,
       title,
       artist,
       album,
