0fc30df9df1a91604018157f8b934b0d5b2eb5a7
Nick Radford <nick@nicholasradford.com>
2023-01-04 17:06:32 -0800

    Only use atom feed, add links in UI

--- links.ts
+++ links.ts
@@ -4,8 +4,8 @@ import {
   faLinkedin,
   IconDefinition,
 } from "@fortawesome/free-brands-svg-icons";
-
 import { faSquareRss } from "@fortawesome/free-solid-svg-icons";
+
 import { BASE_URL } from "./lib/url";
 
 export const links: [url: string, name: string, icon?: IconDefinition][] = [
