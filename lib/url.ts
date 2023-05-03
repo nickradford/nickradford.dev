4377c54d2706fee59487eac5bb2e8e166aa679b2
Nick Radford <nick@nicholasradford.com>
2023-05-03 12:30:51 -0700

    use full process.env string for the BASE_URL

--- lib/url.ts
+++ lib/url.ts
@@ -1,10 +1,7 @@
-const { NEXT_PUBLIC_VERCEL_URL: Vercel, NEXT_PUBLIC_BASE_URL: Base } =
-  process.env;
-
-export const BASE_URL = Base
-  ? Base
-  : Vercel
-  ? `https://${Vercel}`
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+  ? process.env.NEXT_PUBLIC_BASE_URL
+  : process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : `http://localhost:3000`;
 
 console.info("BASE_URL", BASE_URL);
