032da7c846aa9c49a245914900b7aa2bc0bb2ecb
Nick Radford <nick@nicholasradford.com>
2025-12-13 11:42:08 -0800

    cleanup

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,14 +1,4 @@
-const dateFormatter = new Intl.DateTimeFormat("en-US", {
-  dateStyle: "long",
-});
-
-const getLocalDate = (date: Date) => {
+export const getLocalDate = (date: Date) => {
   const offset = date.getTimezoneOffset() * 60000;
   return new Date(date.getTime() + offset);
 };
-
-export function formatDate(date: string | Date): string {
-  const dateObj = typeof date === "string" ? new Date(date) : date;
-  const localDate = getLocalDate(dateObj);
-  return dateFormatter.format(localDate);
-}
