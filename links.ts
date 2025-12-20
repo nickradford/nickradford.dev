aadc3dd6e988ecc70bfb19eabcf3f0a3a8f27d8f
Nick Radford <nick@nicholasradford.com>
2025-12-19 18:10:06 -0800

    Add icon for resume, clean up links, remove phosphor-icons/react dep

--- links.ts
+++ links.ts
@@ -1,9 +1,8 @@
-import { type Icon, GithubLogoIcon, LinkedinLogoIcon, RssIcon } from "@phosphor-icons/react";
 import { BASE_URL } from "./src/lib/url";
 
-export const links: [url: string, name: string, icon?: Icon][] = [
-  ["https://github.com/nickradford", "GitHub", GithubLogoIcon],
-  ["https://www.linkedin.com/in/nickradford/", "LinkedIn", LinkedinLogoIcon],
-  ["/nick-radford-resume-2025-10-30.pdf", "Resume"],
-  [`${BASE_URL}/feeds/atom.xml`, "Feed", RssIcon],
+export const links: [url: string, name: string, icon?: string][] = [
+  ["https://github.com/nickradford", "GitHub"],
+  ["https://www.linkedin.com/in/nickradford/", "LinkedIn"],
+  ["/nick-radford-resume-2025-10-30.pdf", "Resume", "file-pdf"],
+  [`${BASE_URL}/feeds/atom.xml`, "Feed"],
 ];
