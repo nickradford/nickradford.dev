4287bdb111a59f305c6fdd22f81f4d66ed238e9b
Nick Radford <nick@nicholasradford.com>
2026-06-19 01:51:44 -0700

    Update resume download links

--- navigationLinks.ts
+++ navigationLinks.ts
@@ -1,8 +1,8 @@
 import { BASE_URL } from "./src/lib/url";
 
-export const links: [url: string, name: string, icon?: string][] = [
+export const links: [url: string, name: string, icon?: string, download?: string][] = [
   ["https://github.com/nickradford", "GitHub"],
   ["https://www.linkedin.com/in/nickradford/", "LinkedIn"],
-  ["/nick-radford-resume-2025-10-30.pdf", "Resume", "file-pdf"],
+  ["/nick-radford-resume-2026-06-18.pdf", "Resume", "file-pdf", "Nick Radford Resume.pdf"],
   [`${BASE_URL}/feeds/atom.xml`, "Feed"],
 ];
